03068d7f656b038bf63c8de9f1b73beaadc900c3
isharasmax2-lang <isharasmax2@gmail.com>
2026-04-25 16:39:03 +0530

    Merge develop and resolve App/index.css conflicts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import FacilitiesPage from './features/facilities/FacilitiesPage.jsx'
 import LoginPage from './pages/LoginPage'
 
 function App() {
-  return <LoginPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/facilities" element={<FacilitiesPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App

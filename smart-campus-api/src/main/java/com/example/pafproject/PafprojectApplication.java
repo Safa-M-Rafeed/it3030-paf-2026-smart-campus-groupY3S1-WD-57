b617498994a6a2ea0e5bee8f79153a5c084cdaec
isharasmax2-lang <isharasmax2@gmail.com>
2026-04-25 17:00:01 +0530

    Fix backend startup main class and Maven run config

---
+++ smart-campus-api/src/main/java/com/example/pafproject/PafprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.pafproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PafprojectApplication.class, args);
+    }
+}
